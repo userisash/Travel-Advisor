cd181179bf58f8e274a1073ed21a8688c279c735
ashraf araj <ashrafthehero423@gmail.com>
2023-04-12 22:03:26 +0300

    user can add popup

---
+++ Client/MapIt/src/Components/FetchPins.jsx
@@ -0,0 +1,48 @@
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import RoomIcon from '@mui/icons-material/Room';
+import { Marker } from 'react-map-gl';
+import Popups from './Popup';
+import '../../src/Map.css'
+
+function FetchPins(props){
+    const currentUser = 'Mohammed'
+    const [pins, setPins] = useState([])
+    const [showPopup, setShowPopup] = useState(false);
+    const [currentplaceId, setCurrentPlaceId] = useState(null)
+
+   const handleMarkerClick = (id) =>{
+      console.log("Marker clicked:", id);
+      setCurrentPlaceId(id)
+    }
+
+    
+     
+    useEffect(()=>{
+        const getPins = async() => {
+            try {
+              const res = await axios.get('http://localhost:8800/api/pins/pins');
+              setPins(res.data);
+            } catch(err) {
+              console.log(err);
+            }
+          }          
+        getPins()
+    },[])
+    
+    return(
+        <>
+        {pins.map(p=>(
+             <Marker key={p._id} longitude={p.long} latitude={p.lat} layer="top" style={{position:'absolute', top:0, left:0}} >
+                <RoomIcon color='primary' fontSize='large' style={{ width:'40px', height:'40px', color: p.username === currentUser ? "tomato": "blue" }} onClick={()=>setShowPopup(true)} />
+                 {console.log("Popup for:", p._id, "Current place:", currentplaceId)}
+                 {showPopup && (
+                     <Popups longitude={p.long} latitude={p.lat} setShowPopup={setShowPopup} handleMarkerClick={handleMarkerClick} pin={p}></Popups>
+                 )}
+             </Marker>
+        ))}
+        </>
+    )
+}
+export default FetchPins
--- Client/MapIt/src/Components/Map.jsx
+++ Client/MapIt/src/Components/Map.jsx
@@ -1,31 +1,86 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Map, {Marker} from 'react-map-gl';
 import RoomIcon from '@mui/icons-material/Room';
 import Popups from './Popup';
 import '../../src/Map.css'
+import FetchPins from './FetchPins'
+import useHandleAddingPins from '../utils/UseAddPins';
+import  { Popup } from "react-map-gl";
 
-function MapDisplay(){
-  const [showPopup, setShowPopup] = useState(true);
-    return (
-        <Map
-          initialViewState={{
-              longitude: -122.4,
-              latitude: 37.8,
-              zoom: 4
-          }}
-          style={{width: "100vw", height: "100vh" ,position:'relative'}}
-          mapboxAccessToken='pk.eyJ1IjoiYXNocmFmLWFyYWoiLCJhIjoiY2xnM2NuNzFrMDNpeTNkbTJvZXY2c3pqeiJ9.C7iovVKPmBIj4IKUlJ_S_g'
-          mapStyle="mapbox://styles/mapbox/streets-v9"
+function MapDisplay() {
+  const [pins, setPins] = useState([]);
+  const { newPlace, setNewPlace, handlePinClick } = useHandleAddingPins();
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await fetch("http://localhost:8800/api/pins/pins");
+        console.log(res);
+        if (!res.ok) {
+          throw new Error("HTTP Error " + res.status);
+        }
+        const data = await res.json();
+        setPins(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+  return (
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 4,
+      }}
+      style={{ width: "100vw", height: "100vh", position: "relative" }}
+      mapboxAccessToken="pk.eyJ1IjoiYXNocmFmLWFyYWoiLCJhIjoiY2xnM2NuNzFrMDNpeTNkbTJvZXY2c3pqeiJ9.C7iovVKPmBIj4IKUlJ_S_g"
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      onDblClick={handlePinClick}
+    >
+      <FetchPins pins={pins}></FetchPins>
+      {newPlace && (
+        <Popup 
+          longitude={newPlace.lng} 
+          latitude={newPlace.lat}
+          anchor="left"
+          maxWidth="none"
+          className="popup"
+          onClose={() => setNewPlace(null)}
         >
-              <Marker longitude={2.294694} latitude={48.858093} layer="top" style={{position:'absolute', top:0, left:0}} >
-                <RoomIcon color='primary' fontSize='large' style={{ width:'20px', height:'20px'}} />
-             </Marker>
-            {/* {showPopup && (
-              <Popups longitude={2.294694} latitude={48.858093} setShowPopup={setShowPopup}></Popups>
-            )} */}
-        </Map> 
-      );
+         <div>
+                <form className='add-pin card'>
+                  <label>Title</label>
+                  <input
+                    placeholder="Enter a title"
+                    autoFocus
+                    // onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <label>Description</label>
+                  <textarea
+                    placeholder="Say something about this place."
+                    // onChange={(e) => setDesc(e.target.value)}
+                  />
+                  <label>Rating</label>
+                  <select >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <button type="submit" className="submitButton">
+                    Add Pin
+                  </button>
+                </form>
+              </div>
+        </Popup>
+      )}
+    </Map>
+  );
 }
 
 export default MapDisplay
